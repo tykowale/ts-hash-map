4392b184b147367880557068f9cba7d93f8bfcd5
tykowale <tykowale@gmail.com>
2024-02-21 12:38:43 -0600

    update hashes to include random prime numbers

--- src/hash.test.ts
+++ src/hash.test.ts
@@ -1,24 +1,24 @@
 import { getHashCode } from 'src/hash';
 
-describe('HashCode tests', () => {
-  it.each`
+describe('getHashCode tests', () => {
+it.each`
     value                     | expected
-    ${null}                   | ${0}
-    ${undefined}              | ${0}
-    ${42}                     | ${42}
-    ${3.14}                   | ${3.14}
-    ${true}                   | ${1}
-    ${false}                  | ${0}
-    ${'hello'}                | ${99162322} 
-    ${'world'}                | ${113285276} 
-    ${new Date('2022-01-01')} | ${1640995200000} 
-    ${/^hello$/i}             | ${65154666} 
-    ${() => {}}               | ${-1830454922} 
-    ${[1, 2, 3]}              | ${579757003} 
-    ${new Map([[1, 'one'], [2, 'two']])} | ${-577285097} 
-    ${new Set([1, 2, 3])}     | ${8} 
-    ${{}}                     | ${0} 
-    ${{ a: 1, b: 2 }}         | ${-1798626175} 
+  ${null}                   | ${0}
+  ${undefined}              | ${0}
+  ${42}                     | ${42}
+  ${3.14}                   | ${3.14}
+  ${true}                   | ${1231}
+  ${false}                  | ${1237}
+  ${'hello'}                | ${99162322}
+  ${'world'}                | ${113318802}
+  ${new Date('2022-01-01')} | ${1640995200000}
+  ${/^hello$/i}             | ${459043387}
+  ${() => {}}               | ${-702898920}
+  ${[1, 2, 3]}              | ${1026}
+  ${new Map([[1, 'one'], [2, 'two']])} | ${6512020}
+  ${new Set([1, 2, 3])}     | ${6}
+  ${{}}                     | ${0}
+  ${{ a: 1, b: 2 }}         | ${9376} 
   `('returns $expected for $value', ({ value, expected }) => {
     expect(getHashCode(value)).toEqual(expected);
   });
@@ -26,6 +26,6 @@ describe('HashCode tests', () => {
   it('handles circular references', () => {
     const obj: any = {};
     obj.objRef = obj;
-    expect(getHashCode(obj)).toEqual(0);
+    expect(getHashCode(obj)).toEqual(-1023386596);
   });
 });
--- src/hash.ts
+++ src/hash.ts
@@ -21,22 +21,22 @@ function getRegExpHashCode(value: RegExp): number {
   return getStringHashCode(value.toString());
 }
 
-function getFunctionHashCode(value: Function): number {
-  return getStringHashCode(value.toString());
-}
-
 function getArrayHashCode(value: unknown[], refs: unknown[]): number {
   let hash = 0;
+
   for (const item of value) {
-    hash = hash * 31 + getHashCode(item, refs);
+    // 31 is just random large prime. It could really be anything
+    hash = (hash * 31) + getHashCode(item, refs);
   }
+
   return hash;
 }
 
 function getMapHashCode(value: Map<any, unknown>, refs: unknown[]): number {
   let hash = 0;
   for (const [key, val] of value.entries()) {
-    hash ^= getHashCode(key, refs) ^ getHashCode(val, refs);
+    // 59 is just random large prime. It could really be anything
+    hash ^= (hash * 59) + getHashCode(key, refs) ^ getHashCode(val, refs);
   }
   return hash;
 }
@@ -44,37 +44,43 @@ function getMapHashCode(value: Map<any, unknown>, refs: unknown[]): number {
 function getSetHashCode(value: Set<unknown>, refs: unknown[]): number {
   let hash = 0;
   for (const item of value) {
-    hash ^= getHashCode(item, refs);
+    hash += getHashCode(item, refs);
   }
   return hash;
 }
 
 function getObjectHashCode(value: any, refs: any[]): number {
   let hash = 0;
+
   const keys = Object.keys(value).sort();
   for (const key of keys) {
-    hash ^= getStringHashCode(key) ^ getHashCode(value[key], refs);
+    // 97 is just random large prime. It could really be anything
+    hash ^= (hash * 97) + getStringHashCode(key) ^ getHashCode(value[key], refs);
   }
+
   return hash;
 }
 
 export function getHashCode(value: unknown, refs: unknown[] = []): number {
-  // Handle null and undefined
+  // null and undefined are 0
   if (value == null) {
     return 0;
   }
 
-  // Handle primitives with switch statement
+  // a lot of the primitives will just be treated as strings
   switch (typeof value) {
     case 'number':
       return value;
     case 'bigint':
-      // Treat bigint as a string
       return getStringHashCode(value.toString());
     case 'boolean':
       return getBooleanHashCode(value);
     case 'string':
       return getStringHashCode(value);
+    case 'function':
+      return getStringHashCode(value.toString());
+    case 'symbol':
+      return getStringHashCode(value.toString());
     default:
       break;
   }
@@ -93,8 +99,6 @@ export function getHashCode(value: unknown, refs: unknown[] = []): number {
       return getDateHashCode(value);
     } else if (value instanceof RegExp) {
       return getRegExpHashCode(value);
-    } else if (value instanceof Function) {
-      return getFunctionHashCode(value);
     } else if (value instanceof Map) {
       return getMapHashCode(value, refs);
     } else if (value instanceof Set) {
