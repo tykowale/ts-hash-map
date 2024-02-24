6049f4ce83c9d4b56d02410396c8fdaf5354f949
tykowale <tykowale@gmail.com>
2024-02-24 09:03:59 -0600

    clean up benchmarking a bit

--- src/benchmark.ts
+++ src/benchmark.ts
@@ -1,63 +1,93 @@
 import { HashMap } from './hash-map';
 
 const size = 1000000;
+const keys = generateRandomKeys();
+const results: any[] = [];
+
+function getRandomInt(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateRandomKeys(): number[] {
+  const keys: number[] = [];
+  for (let i = 0; i < size; i++) {
+    keys.push(getRandomInt(1, Number.MAX_SAFE_INTEGER));
+  }
+  return keys;
+}
+
+function benchmarkOperation(title: string, operation: () => void) {
+  console.log(`Testing ${title}`);
+  const startTime = new Date().getTime();
+  operation();
+  const endTime = new Date().getTime();
+  const totalTime = endTime - startTime;
+  const fnTime = (totalTime / size).toFixed(10);
+  const operations = Math.floor(1000 / totalTime * size);
+  results.push({
+    Title: title,
+    'Total Time (ms)': totalTime,
+    'Time per Operation (ms)': fnTime,
+    'Operations per Second': operations.toLocaleString(),
+  });
+}
 
 function testHashMap(): void {
-  console.log('Testing hash map set');
-  let startTime = new Date().getTime();
   const hashMap = new HashMap<number, string>();
 
-  for (let i = 0; i < size; i++) {
-    hashMap.set(i, `value_${i}`);
-  }
+  benchmarkOperation('hash map set', () => {
+    keys.forEach(key => {
+      hashMap.set(key, `value_${key}`);
+    });
+  });
 
-  let endTime = new Date().getTime();
-  let totalTime = endTime - startTime;
-  let fnTime = (totalTime / size).toFixed(10);
-  let operations = Math.floor(1000 / totalTime * size);
-  console.log(`Hash Map Set total: ${totalTime}ms, time: ${fnTime}, op/sec: ${operations}`);
+  benchmarkOperation('hash map get', () => {
+    keys.forEach(key => {
+      hashMap.get(key);
+    });
+  });
 
+  benchmarkOperation('hash map update', () => {
+    keys.forEach(key => {
+      hashMap.set(key, `new_value_${key}`);
+    });
+  });
 
-  console.log('Testing hash map get');
-  startTime = new Date().getTime();
-  for (let i = 0; i < size; i++) {
-    hashMap.get(i);
-  }
-
-  endTime = new Date().getTime();
-  totalTime = endTime - startTime;
-  fnTime = (totalTime / size).toFixed(10);
-  operations = Math.floor(1000 / totalTime * size);
-  console.log(`Hash Map get total: ${totalTime}ms, time: ${fnTime}, op/sec: ${operations}`);
+  benchmarkOperation('hash map delete', () => {
+    keys.forEach(key => {
+      hashMap.delete(key);
+    });
+  });
 }
 
 function testMap(): void {
-  console.log('Testing native map set');
-  let startTime = new Date().getTime();
   const m = new Map<number, string>();
 
-  for (let i = 0; i < size; i++) {
-    m.set(i, `value_${i}`);
-  }
+  benchmarkOperation('native map set', () => {
+    keys.forEach(key => {
+      m.set(key, `value_${key}`);
+    });
+  });
 
-  let endTime = new Date().getTime();
-  let totalTime = endTime - startTime;
-  let fnTime = (totalTime / size).toFixed(10);
-  let operations = Math.floor(1000 / totalTime * size);
-  console.log(`Native Map Set total: ${totalTime}ms, time: ${fnTime}, op/sec: ${operations}`);
+  benchmarkOperation('native map get', () => {
+    keys.forEach(key => {
+      m.get(key);
+    });
+  });
 
+  benchmarkOperation('native map update', () => {
+    keys.forEach(key => {
+      m.set(key, `new_value_${key}`);
+    });
+  });
 
-  console.log('Testing native map get');
-  startTime = new Date().getTime();
-  for (let i = 0; i < size; i++) {
-    m.get(i);
-  }
-
-  endTime = new Date().getTime();
-  totalTime = endTime - startTime;
-  fnTime = (totalTime / size).toFixed(10);
-  operations = Math.floor(1000 / totalTime * size);
-  console.log(`Native Map get total: ${totalTime}ms, time: ${fnTime}, op/sec: ${operations}`);
+  benchmarkOperation('native map delete', () => {
+    keys.forEach(key => {
+      m.delete(key);
+    });
+  });
 }
 
 function runTests(): void {
@@ -65,6 +95,8 @@ function runTests(): void {
   testHashMap();
   testMap();
   console.log('Tests finished!');
+  console.log('Benchmark Results:');
+  console.table(results);
 }
 
 runTests();
